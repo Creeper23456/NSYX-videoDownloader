85378f3f0cb50ddfa445ca0f17d0ab2631e2d449
Creeper Null <40138690+Creeper23456@users.noreply.github.com>
2021-11-06 13:26:52 +0800

    Add files via upload

--- VideoDownloader.bat
+++ VideoDownloader.bat
@@ -9,11 +9,12 @@ set output="C:\Users\Administrator\Desktop"
 set runtime_tmp="C:\runtime_tmp"
 set net_test_addr=www.baidu.com
 set um_path="D:\CloudMusic"
+set msg=wscript.exe "D:\RunTimePath\batch\msg.vbs"
+set Version=beta3
 
 ::设定AMV转换器参数
 
 set amv_trans_arg=
-
 
 goto network_check
 
@@ -22,6 +23,7 @@ title 网络监测 - 视频下载、转换器
 echo 正在检查网络连接性...
 ping %net_test_addr% /n 2 >nul
 if %errorlevel% == 1 (set err_rea=网络监测失败！无法连接至服务器。&&goto error)
+%msg% 注意：使用视频下载功能时，杀毒软件可能会报毒，请添加信任。
 cls
 REM 开始执行
 goto start
@@ -32,8 +34,8 @@ md %runtime_tmp% >nul
 cls
 goto start_1
 :start_1
-set start_var=
-set about_var=
+set start_var=START
+set about_var=START
 color 0f
 title 视频下载、转换器 - 主菜单
 echo =====欢迎使用视频下载转换器=====
@@ -47,15 +49,16 @@ echo ================================
 echo 本程序支持bilibili，优酷，爱奇艺或YouTube等常见平台下载。
 set /p start_var=请输入对应功能序号：
 if %start_var% == 1 (goto video2mp3)
-if %start_var% == 0 (goto about)
+::if %start_var% == 0 (goto about)
+if %start_var% == 0 (%msg% 简易BATCH脚本下载程序，基于Annie程序与FFmpeg实现。版本号:%Version%，Creeper23456制作。PigeonClub技术组版权所有。 &&goto about)
 if %start_var% == 3 (goto amvtrans)
-if %start_var% == 2 (goto videodl) else (cls && echo 输入了错误的序号！ && goto start_1)
-if %start_var% ==  (goto start_1)
+if %start_var% == 2 (goto videodl) else (cls && echo 输入了错误的序号！ &&goto start_1)
 
 :video2mp3
 cls
 del /q /s %tmp%\* >nul
 title 下载视频...
+echo 请保证输入的地址以https://开头。
 set /p download_url=输入bv或av号，非b站视频请输入地址（在浏览器中复制）：
 annie -o %tmp% %download_url%
 if %errorlevel% == 1 (set err_rea=视频不存在、视频链接有误或网络出现问题！&&goto error)
@@ -104,16 +107,19 @@ echo 简易BATCH脚本下载程序，基于Annie程序与FFmpeg实现。
 echo 由PigeonClub技术部Creeper23456编写，官网：https://club.cnklp.cn
 echo 本程序依赖于RunTimePath自启程序支持，请不要修改启动项或注册表。
 echo 我的个人网站：https://blog.cnklp.cn
-echo 版本1.0beta
+echo 版本%Version%
 echo 输入指令可以执行以下操作：
 echo update：更新本程序
 echo website：查看我们的官方网站
 echo blog：查看我的个人博客
-set /p about_var=键入你想继续的操作，回车返回主菜单：
-rem if %about_var% == update (goto update)
-if %about_var% == website (explorer https://club.cnklp.cn)
-rem if %about_var% == blog (start https://blog.cnklp.cn)
-goto start
+echo repo：查看本程序源代码(Gtihub)
+echo back：返回主菜单
+set /p about_var=键入你想继续的操作，回车确认：
+if %about_var% == update (goto update)
+if %about_var% == website (start https://club.cnklp.cn)
+if %about_var% == repo (start https://github.com/Creeper23456/NSYX-videoDownloader/)
+if %about_var% == back (goto start)
+if %about_var% == blog (start https://blog.cnklp.cn) else (goto about)
 
 :error
 cls
@@ -156,6 +162,11 @@ echo 转换耗时较长，请保持程序运行。
 set /p amv_input=输入被转换视频文件的路径，或拖入文件到此窗口：
 cls
 echo 初始化...
+echo 复制文件到缓存文件夹...
+copy %amv_input% %tmp%
+dir /b /on %tmp%>%runtime_tmp%\list.txt
+set /p trans_filename=<%runtime_tmp%\list.txt
+
 set  amv_trans_arg=%amv_trans_arg% -y
 set  amv_trans_arg=%amv_trans_arg% -hide_banner
 set  amv_trans_arg=%amv_trans_arg% -threads auto
@@ -164,7 +175,7 @@ set  amv_trans_arg=%amv_trans_arg% -err_detect ignore_err
 set  amv_trans_arg=%amv_trans_arg% -flags "-output_corrupt"
 set  amv_trans_arg=%amv_trans_arg% -fflags "+discardcorrupt+autobsf"
 set  amv_trans_arg=%amv_trans_arg% -flags2 "+ignorecrop"
-set  amv_trans_arg=%amv_trans_arg% -i %amv_input%
+set  amv_trans_arg=%amv_trans_arg% -i %tmp%\%trans_filename%
 set  amv_trans_arg=%amv_trans_arg% -movflags "+faststart"
 set  amv_trans_arg=%amv_trans_arg% -qscale:v 2
 set amv_trans_arg=%amv_trans_arg% -bsf:v "mpeg4_unpack_bframes,remove_extra=freq=all"
@@ -181,10 +192,16 @@ set  amv_trans_arg=%amv_trans_arg%,scale=208:176
 set  amv_trans_arg=%amv_trans_arg%,eq=gamma=1.2:saturation=1.1
 set  amv_trans_arg=%amv_trans_arg%,hqdn3d"
 set  amv_trans_arg=%amv_trans_arg% -pix_fmt yuv420p
+
 cls
 title 正在转换...
-ffmpeg %amv_trans_arg% %output%\amvoutput.avi
+ffmpeg %amv_trans_arg% %output%\%trans_filename%.avi
+
 if %errorlevel% == 1 (set err_rea=转换时出现了一个错误！可能视频不受支持或路径错误&&goto error) 
 echo 成功！按任意键回到主菜单，文件已被保存在桌面。
 pause
 goto start
+
+:update
+%msg% 功能完善中！
+goto about
